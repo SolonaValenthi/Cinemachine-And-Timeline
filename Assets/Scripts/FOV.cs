acea3cde7cc5432669eebbc96e2e326f89c8a319
Nikolai Nelson <nikolainelson@comcast.net>
2023-11-22 05:37:22 -0800

    created input controls to change fov/lookat target

--- Assets/Scripts/FOV.cs
+++ Assets/Scripts/FOV.cs
@@ -5,19 +5,51 @@ using Cinemachine;
 
 public class FOV : MonoBehaviour
 {
+    [SerializeField] private Transform _cube;
+    [SerializeField] private Transform _capsule;
+    
     private CinemachineVirtualCamera _vCam;
 
     // Start is called before the first frame update
     void Start()
     {
         _vCam = GetComponent<CinemachineVirtualCamera>();
-        _vCam.m_Lens.FieldOfView = 40;
-        _vCam.Priority = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FOVChange();
+        TargetChange();
+    }
+
+    private void FOVChange()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_vCam.m_Lens.FieldOfView >= 60)
+            {
+                _vCam.m_Lens.FieldOfView = 20;
+            }
+            else
+            {
+                _vCam.m_Lens.FieldOfView += 20;
+            }
+        }
+    }
+
+    private void TargetChange()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (_vCam.LookAt == _cube)
+            {
+                _vCam.LookAt = _capsule;
+            }
+            else
+            {
+                _vCam.LookAt = _cube;
+            }
+        }
     }
 }
