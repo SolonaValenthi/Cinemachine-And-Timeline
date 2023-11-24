bb09d7ec3cb79a6a72ee311cd5a1ebe94a1ed1b8
Nikolai Nelson <nikolainelson@comcast.net>
2023-11-24 11:29:46 -0800

    implemented character camera switching via mouse input

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class Character : MonoBehaviour
     [SerializeField] private CharacterController _controller;
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _rotateSpeed;
+    [SerializeField] private CinemachineVirtualCamera _3rdPerson;
+    [SerializeField] private CinemachineVirtualCamera _orbital;
 
     private Vector3 _direction;
 
@@ -19,15 +22,34 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TankMovement();
+        CameraSwitch();
+    }
+
+    private void TankMovement()
+    {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         _direction = new Vector3(0, 0, vertical);
         _direction *= _moveSpeed * Time.deltaTime;
         _direction = this.transform.TransformDirection(_direction);
         _controller.Move(_direction);
-        
+
 
         Vector3 rotation = new Vector3(0, horizontal * _rotateSpeed * Time.deltaTime, 0);
         this.transform.Rotate(rotation);
+    }
+
+    private void CameraSwitch()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _orbital.Priority = 100;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            _orbital.Priority = 10;
+        }
     }
 }
