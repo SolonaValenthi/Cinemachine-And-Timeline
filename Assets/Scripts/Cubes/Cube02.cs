38693f5406536086dcebdb4d2c2be380bdcaa2eb
Nikolai Nelson <nikolainelson@comcast.net>
2023-12-23 05:12:37 -0800

    signal emmitter article

---
+++ Assets/Scripts/Cubes/Cube02.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube02 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CubeTwo()
+    {
+        Debug.Log("This is Cube_02");
+    }
+}
