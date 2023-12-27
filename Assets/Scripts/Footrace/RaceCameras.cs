521c324078ddf9766d7581971da845dc9df44526
Nikolai Nelson <nikolainelson@comcast.net>
2023-12-27 04:49:19 -0800

    footrace particle system setup

--- Assets/Scripts/Footrace/RaceCameras.cs
+++ Assets/Scripts/Footrace/RaceCameras.cs
@@ -9,6 +9,8 @@ public class RaceCameras : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera _finishLineCam;
     [SerializeField] private CinemachineVirtualCamera _endingCam;
 
+    [SerializeField] private ParticleSystem[] _confetti;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +35,17 @@ public class RaceCameras : MonoBehaviour
 
     public void SwitchToEnd()
     {
+        _raceCam.Priority = 10;
+        _finishLineCam.Priority = 10;
+        _endingCam.Priority = 100;
         Time.timeScale = 1.0f;
+    }
+
+    public void LaunchConfetti()
+    {
+        foreach (var sys in _confetti)
+        {
+            sys.Play();
+        }
     }
 }
