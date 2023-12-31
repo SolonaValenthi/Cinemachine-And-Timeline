856442be336f5a48db0d20b11c7a608f18a321d2
Nikolai Nelson <nikolainelson@comcast.net>
2023-12-31 03:31:26 -0800

    created cinematic timeline

--- Assets/Scripts/Cinematic/CSTrigger.cs
+++ Assets/Scripts/Cinematic/CSTrigger.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CSTrigger : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector _director;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _director.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _director.Play();
+            this.gameObject.SetActive(false);
+        }
     }
 }
